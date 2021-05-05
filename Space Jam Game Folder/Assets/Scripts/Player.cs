824bbc09c46834bb1982e2e199896f25db4add03
gseven-gdv <zerogravitystd@gmail.com>
2021-05-05 18:07:52 -0300

    agora  o player anda

---
+++ Space Jam Game Folder/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField] private Camera camera;
+
+    [SerializeField] private float moveSpeed;
+
+    private Vector2 movement;
+    private Vector2 mousePos;
+
+    private float xAxis, yAxis;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start() 
+    {
+        
+    }
+
+    void Update()
+    {
+        GetInput();
+
+        movement.x = xAxis;
+        movement.y = yAxis;
+
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate() 
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+
+        rb.rotation = angle;
+    }
+
+    private void GetInput() 
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+    }
+}
