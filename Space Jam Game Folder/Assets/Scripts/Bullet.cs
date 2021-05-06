2a4c197151d86aa62dbc26a524f1af04adc63510
gseven-gdv <zerogravitystd@gmail.com>
2021-05-05 21:26:28 -0300

    Agoras as armas atiram (:

---
+++ Space Jam Game Folder/Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+        StartCoroutine(DestroyBulletOnTimer(3f));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (!other.CompareTag("Player")) {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator DestroyBulletOnTimer(float timer) {
+        yield return new WaitForSeconds(timer);
+
+        Destroy(gameObject);
+    }
+}
--- Space Jam Game Folder/Assets/Scripts/Weapon.cs
+++ Space Jam Game Folder/Assets/Scripts/Weapon.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public GameObject bullet;
+
+    [SerializeField] private Transform gunTip;
+
     [SerializeField] private Camera camera;
     [SerializeField] private Rigidbody2D playerPos;
 
@@ -23,21 +27,33 @@ public class Weapon : MonoBehaviour
     {
         HandleRotation();
 
+        if (Input.GetButtonDown("Fire1"))
+            Shoot();
+    }
+
+    private void Shoot() 
+    {
+        Instantiate(bullet, gunTip.position, transform.rotation);
+    }
+
+    private void HandleRotation() 
+    {
         mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
 
         lookDir = mousePos - rb.position;
         angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
 
-        rb.rotation = angle;        
-    }
+        rb.rotation = angle;
 
-    private void HandleRotation() {
         float absRotation = Mathf.Abs(angle);
 
         if (
-            (absRotation <= 69 && absRotation >= 92) ||
-            (absRotation >=-69 && absRotation <=-92)
+            (absRotation >= 69 && absRotation <= 92) ||
+            (absRotation <=-69 && absRotation >=-92)
         ) { 
+            rb.position = playerPos.position;
+        }
+        else {
             rb.position = (absRotation > 90 || absRotation < -90) ?
                 new Vector2(playerPos.position.x + -0.5f, playerPos.position.y) :
                 new Vector2(playerPos.position.x + 0.5f, playerPos.position.y);
