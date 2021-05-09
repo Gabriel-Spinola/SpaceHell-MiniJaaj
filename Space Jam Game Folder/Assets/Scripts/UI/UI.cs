bae7d0c848c2ecfcbff019b0a74daaa1f53f6ae8
gseven-gdv <zerogravitystd@gmail.com>
2021-05-09 18:56:23 -0300

    a

---
+++ Space Jam Game Folder/Assets/Scripts/UI/UI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIplayer {
+    public bool isAlive;
+    public int life;
+
+    //isAlive = 0
+
+}
